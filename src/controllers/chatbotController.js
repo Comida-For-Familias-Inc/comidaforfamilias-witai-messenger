4a8c9533d10895f13a15defda0de96dd2b7356f5
Tongre001 <amendieta704@gmail.com>
2021-05-07 03:31:07 -0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -35,29 +35,37 @@ let getWebhook = (req, res) => {
 }
 
 let postWebhook = (req, res) => {
-  // Parse the request body from the POST
-  let body = req.body;
-console.log(body);
-  // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
+    let body = req.body;
 
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
 
-      // Get the webhook event. entry.messaging is an array, but 
-      // will only ever contain one event, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-      
-    });
+        body.entry.forEach(function (entry) {
 
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
 
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 }
 
 module.exports = {
