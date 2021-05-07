b630cdeb571ca76d82da25c971fdd6edec88e61c
Tongre001 <amendieta704@gmail.com>
2021-05-07 01:35:34 -0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -3,62 +3,62 @@ require("dotenv").config();
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 console.log(FB_VERIFY_TOKEN)
 
-let test=(req, res)=>{
+let test = (req, res) => {
     return res.send("HELLO AGIAN");
 }
 
-let getWebhook = (req, res) =>{
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = FB_VERIFY_TOKEN;
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+let getWebhook = (req, res) => {
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = FB_VERIFY_TOKEN;
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
-  }
 }
 
 let postWebhook = (req, res) => {
     let body = req.body;
-  
+
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
-  
-      // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
-  
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
+
+
+        // Gets the body of the webhook event
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
-      });
-  
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
+
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
     } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
     }
-  
+
 }
 
 module.exports = {
-    test:test,
+    test: test,
     getWebhook: getWebhook,
     postWebhook: postWebhook
 }
@@ -75,5 +75,5 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+
 }
