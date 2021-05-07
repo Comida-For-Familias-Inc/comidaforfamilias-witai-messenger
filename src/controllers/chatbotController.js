48683cd7e59cf3a6ab1ca38e4bed502a0a8d7a8f
Tongre001 <amendieta704@gmail.com>
2021-05-07 04:00:35 -0700

    updating nlp

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -151,7 +151,7 @@ function callSendAPI(sender_psid, response) {
         "recipient": {
             "id": sender_psid
         },
-        "message": response
+        "message": {"text":response}
     }
 
     // Send the HTTP request to the Messenger Platform
