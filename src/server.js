7c057769e2e8a59d9b3d03a05e13a6d047a87070
Tongre001 <amendieta704@gmail.com>
2021-05-07 01:27:53 -0700

    update

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 //init web routes
 initWebRoutes(app);
 
-let port = process.env.port || 8080;
+let port = process.env.PORT || 8080;
 
 app.listen(port, ()=>{
     console.log("App is running at the port: "+ port)
