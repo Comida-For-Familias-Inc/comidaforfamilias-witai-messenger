82b3c00ea0878abbf1a96c379812e82a23884db7
Antonio Mendieta <>
2021-05-08 22:30:51 +0000

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,85 +1,88 @@
 require("dotenv").config();
 import request from "request";
 import axios from "axios";
+import audioLoader from "audio-loader";
+// var load = require('audio-loader')
+
 
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 const WIT_TOKEN = process.env.WIT_TOKEN;
 console.log(FB_VERIFY_TOKEN)
 
 let test = (req, res) => {
-    return res.send("HELLO World");
+  return res.send("HELLO World");
 }
 
 let getWebhook = (req, res) => {
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = FB_VERIFY_TOKEN;
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = FB_VERIFY_TOKEN;
 
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
 
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
 
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
 
-        } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
     }
+  }
 }
 
 let postWebhook = (req, res) => {
-    //extracts from event
-    let body = req.body;
-    //upload file
-    // let upFile = req.files.myfile;
-    //   console.log("file uploaded:");
-    // console.log(upFile);
-    // var buffer = upFile.data;
+  //extracts from event
+  let body = req.body;
+  //upload file
+  // let upFile = req.files.myfile;
+  //   console.log("file uploaded:");
+  // console.log(upFile);
+  // var buffer = upFile.data;
 
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
 
-        body.entry.forEach(function (entry) {
+    body.entry.forEach(function (entry) {
 
-            // Gets the body of the webhook event
-            let webhook_event = entry.messaging[0];
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
 
-            // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
 
-            // Check if the event is a message or postback and
-            // pass the event to the appropriate handler function
-            if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message);
-            } else if (webhook_event.postback) {
-                handlePostback(sender_psid, webhook_event.postback);
-            }
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
 
-        });
+    });
 
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 }
 
 
 
 module.exports = {
-    test: test,
-    getWebhook: getWebhook,
-    postWebhook: postWebhook
+  test: test,
+  getWebhook: getWebhook,
+  postWebhook: postWebhook
 };
 
 /*
@@ -138,55 +141,55 @@ function handleMessage(sender_psid, received_message) {
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
-    let response;
+  let response;
 
-    // Get the payload for the postback
-    let payload = received_postback.payload;
+  // Get the payload for the postback
+  let payload = received_postback.payload;
 
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-        response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image." }
-    }
-    // Send the message to acknowledge the postback
-    callSendAPI(sender_psid, response);
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
 }
 
 function callSendAPI(sender_psid, response) {
-    // Construct the message body
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": {"text": response}
-    }
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": { "text": response }
+  }
 
-    // Send the HTTP request to the Messenger Platform
-    request({
-        "uri": "https://graph.facebook.com/v10.0/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-    });
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v10.0/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
 }
 
 //Handling Natural language processing
 function firstTrait(nlp, name) {
-    return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
+  return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
 }
 
 function handleMessage(sender_psid, received_message) {
   console.log("Start of handleMessage");
   let response;
 
- // Checks if the message contains text
+  // Checks if the message contains text
   if (received_message.text) {
     // Creates the payload for a basic text message, which
     // will be added to the body of our request to the Send API
@@ -197,31 +200,40 @@ function handleMessage(sender_psid, received_message) {
     // Gets the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
     console.log("attachment_url", attachment_url);
-    let audioObj = new Audio(attachment_url);
+    // let audioObj = new Audio(attachment_url);
+
+
 
     const url = "https://api.wit.ai/speech";
     const witToken = process.env.WIT_TOKEN; //don't put your token inline
-    console.log("audioObj", audioObj);
+    // console.log("audioObj", audioObj);
 
-    axios
-      .post(url, audioObj.data, {
-        headers: {
-          Authorization: "Bearer " + witToken,
-          "Content-Type": "audio/wav"
-        }
-      })
+    audioLoader(attachment_url).then(function (buffer) {
+      // buffer is an AudioBuffer
+      // play(buffer)
+      console.log("buffer", buffer);
 
-      .then(witResponse => {
-        console.log("wit response: " + JSON.stringify(witResponse.data));
-        // res.json(witResponse.data);
-      })
+      axios
+        .post(url, buffer, {
+          headers: {
+            Authorization: "Bearer " + witToken,
+            "Content-Type": "audio/wav"
+          }
+        })
 
-      .catch(e => {
-        console.log("error sending to wit: " + e);
-        // res.json({ error: e.message });
-      });
-    } 
-  
+        .then(witResponse => {
+          console.log("wit response: " + JSON.stringify(witResponse.data));
+          // res.json(witResponse.data);
+        })
+
+        .catch(e => {
+          console.log("error sending to wit: " + e);
+          // res.json({ error: e.message });
+        });
+    }
+    });
+
+
   // Sends the response message
   // callSendAPI(sender_psid, response); 
 
@@ -232,9 +244,9 @@ function handleMessage(sender_psid, received_message) {
   console.log("received_message", received_message);
   // console.log("CallSENDAPI", callSendAPI());
   if (greeting && greeting.confidence > 0.8) {
-      callSendAPI(sender_psid,'Hi there!');
+    callSendAPI(sender_psid, 'Hi there!');
   } else {
-      // default
-      callSendAPI(sender_psid, 'default');
+    // default
+    callSendAPI(sender_psid, 'default');
   }
 }
