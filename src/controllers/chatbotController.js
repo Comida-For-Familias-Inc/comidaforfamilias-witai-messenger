e9aceec5ad2e69bde8d4345dca5ef689a6560841
Antonio Mendieta <>
2021-05-08 21:20:23 +0000

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -51,7 +51,7 @@ let postWebhook = (req, res) => {
 
             // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
-find
+
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
