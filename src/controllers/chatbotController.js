43ed8aba6b8700b26e88f19b3880bc91e41b83d3
Antonio Mendieta <>
2021-05-09 01:52:21 +0000

    CHANGED THE CODE TO AN EXAMPLE ON GITHUB

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,368 +1,215 @@
-require("dotenv").config();
-import request from "request";
-import axios from "axios";
-// import audioLoader from "audio-loader";
-// var load = require('audio-loader')
-import createBuffer from 'audio-buffer-from';
-// var createBuffer = require('audio-buffer-from')
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ */
 
-const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+'use strict';
+
+// Messenger API integration example
+// We assume you have:
+// * a Wit.ai bot setup (https://wit.ai/docs/quickstart)
+// * a Messenger Platform setup (https://developers.facebook.com/docs/messenger-platform/quickstart)
+// You need to `npm install` the following dependencies: body-parser, express, node-fetch.
+//
+// 1. npm install body-parser express node-fetch
+// 2. Download and install ngrok from https://ngrok.com/download
+// 3. ./ngrok http 8445
+// 4. WIT_TOKEN=your_access_token FB_APP_SECRET=your_app_secret FB_PAGE_TOKEN=your_page_token node examples/messenger.js
+// 5. Subscribe your page to the Webhooks using verify_token and `https://<your_ngrok_io>/webhook` as callback URL.
+// 6. Talk to your bot on Messenger!
+
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const express = require('express');
+const fetch = require('node-fetch');
+
+let Wit = null;
+let log = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  log = require('../').log;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  log = require('node-wit').log;
+}
+
+// Webserver parameter
+const PORT = process.env.PORT || 8445;
+
+// Wit.ai parameters
 const WIT_TOKEN = process.env.WIT_TOKEN;
 
-//wit_handler
-const wit_handler = require("./wit_handler");
+// Messenger API parameters
+const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+if (!FB_PAGE_TOKEN) { throw new Error('missing FB_PAGE_TOKEN') }
+const FB_APP_SECRET = process.env.FB_APP_SECRET;
+if (!FB_APP_SECRET) { throw new Error('missing FB_APP_SECRET') }
 
-let test = (req, res) => {
-  return res.send("HELLO World");
-}
+let FB_VERIFY_TOKEN = null;
+crypto.randomBytes(8, (err, buff) => {
+  if (err) throw err;
+  FB_VERIFY_TOKEN = buff.toString('hex');
+  console.log(`/webhook will accept the Verify Token "${FB_VERIFY_TOKEN}"`);
+});
 
-let getWebhook = (req, res) => {
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = FB_VERIFY_TOKEN;
+// ----------------------------------------------------------------------------
+// Messenger API specific code
 
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+// See the Send API reference
+// https://developers.facebook.com/docs/messenger-platform/send-api-reference
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+const fbMessage = (id, text) => {
+  const body = JSON.stringify({
+    recipient: { id },
+    message: { text },
+  });
+  const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
+  return fetch('https://graph.facebook.com/me/messages?' + qs, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body,
+  })
+  .then(rsp => rsp.json())
+  .then(json => {
+    if (json.error && json.error.message) {
+      throw new Error(json.error.message);
     }
-  }
-}
-
-let postWebhook = (req, res) => {
-  //extracts from event
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    body.entry.forEach(function (entry) {
-
-      // Gets the body of the webhook event
-      let webhook_event = entry.messaging[0];
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);
-      } else if (webhook_event.postback) {
-        handlePostback(sender_psid, webhook_event.postback);
-      }
-
-    });
-
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-}
-
-
-
-module.exports = {
-  test: test,
-  getWebhook: getWebhook,
-  postWebhook: postWebhook
+    return json;
+  });
 };
 
-/*
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
+// ----------------------------------------------------------------------------
+// Wit.ai bot specific code
 
-    let response;
+// This will contain all user sessions.
+// Each session has an entry:
+// sessionId -> {fbid: facebookUserId, context: sessionState}
+const sessions = {};
 
-    // Check if the message contains text
-    if (received_message.text) {
-
-        // Create the payload for a basic text message
-        response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
-        }
-    } else if (received_message.attachments) {
-
-        // Gets the URL of the message attachment
-        let attachment_url = received_message.attachments[0].payload.url;
-
-
-        response = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Is this the right picture?",
-                        "subtitle": "Tap a button to answer.",
-                        "image_url": attachment_url,
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Yes!",
-                                "payload": "yes",
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No!",
-                                "payload": "no",
-                            }
-                        ],
-                    }]
-                }
-            }
-        }
-
-
-    }
-
-    // Sends the response message
-    callSendAPI(sender_psid, response);
-}
-*/
-
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-  let response;
-
-  // Get the payload for the postback
-  let payload = received_postback.payload;
-
-  // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
-    response = { "text": "Oops, try sending another image." }
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
-}
-
-function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": { "text": response }
-  }
-
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v10.0/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
+const findOrCreateSession = (fbid) => {
+  let sessionId;
+  // Let's see if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // Yep, got it!
+      sessionId = k;
     }
   });
-}
-
-//Handling Natural language processing
-function firstTrait(nlp, name) {
-  return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
-}
-
-
-function handleMessage(sender_psid, message) {
-  // check greeting is here and is confident
-  //console.log(message.intents);
-  const greeting0 = firstTrait(message.nlp, 'wit$greetings');
-  const greeting2 = firstTrait(message.nlp, 'afternoon_greeting');
-  
-  console.log("TONY TEST");
-  console.log("GREETING0: ", greeting0)
-  console.log("GREETING2: ", greeting2)
-  console.log("MESSAGE: ", message)
-  console.log("MESSAGE.nlp: ", message.nlp)
-  console.log("MESSAGE.NLP.INTENT", message.nlp.intents);
-  console.log("END OF TONY TEST");
-
-  console.log("data from wit:");
-  console.log(JSON.stringify(message));
-  //const intent = message.nlp.intents.length > 0 && message.nlp.intents[0] || "__foo__";
-
-  const intent = message.nlp.intents
-  console.log("INTENT IS   ", intent)
-  console.log(intent.name);
-
-  switch (intent.name) {
-      case "afternoon_greeting":
-        result = "Good afternoon!";
-      case "bye":
-        result = "Thank you for your interest in Comida For Familias. Have a great day!";
-      case "donate":
-        result = handleDonate(message);
-      case "evening_greeting":
-        result = "Good evening!";
-      case "get_job_list":
-        result = handleGetJobList(message);
-      case "get_location":
-        result = handleGetLocation(message);
-      case "get_news":
-        result = handleGetNews(message);
-      case "get_projects":
-        result = handleGetProjects(message);
-      case "greetings":
-        result = "Hello! Welcome to the Facebook page of Comida For Familias.";
-      case "introduction":
-        result = handleIntroduction(message);
-      case "join_volunteer":
-        result = handleJoinVolunteer(message);
-      case "morning_greeting":
-        result = "Good morning!";
-      case "no_prob":
-        result = "You are very welcome.";
-      case "opt_cpt":
-        result = handleOptCpt(message);
-      case "organization_purpose":
-        result = handleOrganizationPurpose(message);
-    }
-    
-    //return handleGibberish();
-  /*const greeting = firstTrait(message.nlp, 'wit$greetings');
-  if (greeting && greeting.confidence > 0.8) {
-    callSendAPI(sender_psid,'Hi there!');
-  } else { 
-    // default logic
-    callSendAPI(sender_psid,'default');
-  }*/
-  callSendAPI(sender_psid, result)
-}
-
-
-function handleGibberish() {
-    return Promise.resolve(
-      "ask me something like 'How do I join as a volunteer?' or 'What is Comida For Familias for?'"
-    );
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {fbid: fbid, context: {}};
   }
+  return sessionId;
+};
 
-//testing functions
-function handleDonate(data){
-  return "Test"
-}
+// Setting up our bot
+const wit = new Wit({
+  accessToken: WIT_TOKEN,
+  logger: new log.Logger(log.INFO)
+});
 
-function handleGetJobList(data){
-  return "Test"
-}
+// Starting our webserver and putting it all together
+const app = express();
+app.use(({method, url}, rsp, next) => {
+  rsp.on('finish', () => {
+    console.log(`${rsp.statusCode} ${method} ${url}`);
+  });
+  next();
+});
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
-function handleGetLocation(data){
-  return "Test"
-}
+// Webhook setup
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
 
-function handleGetNews(data){
-  return "Test"
-}
-function handleGetProjects(data){
-  return "Test"
-}
-function handleIntroduction(data){
-  return "Test"
-}
-function handleJoinVolunteer(data){
-  return "Test"
-}
-function handleOptCpt(data){
-  return "Test"
-}
-function handleOrganizationPurpose(data){
-  return "Test"
-}
+// Message handler
+app.post('/webhook', (req, res) => {
+  // Parse the Messenger payload
+  // See the Webhook reference
+  // https://developers.facebook.com/docs/messenger-platform/webhook-reference
+  const data = req.body;
+
+  if (data.object === 'page') {
+    data.entry.forEach(entry => {
+      entry.messaging.forEach(event => {
+        if (event.message && !event.message.is_echo) {
+          // Yay! We got a new message!
+          // We retrieve the Facebook user ID of the sender
+          const sender = event.sender.id;
+
+          // We could retrieve the user's current session, or create one if it doesn't exist
+          // This is useful if we want our bot to figure out the conversation history
+          // const sessionId = findOrCreateSession(sender);
+
+          // We retrieve the message content
+          const {text, attachments} = event.message;
+
+          if (attachments) {
+            // We received an attachment
+            // Let's reply with an automatic message
+            fbMessage(sender, 'Sorry I can only process text messages for now.')
+            .catch(console.error);
+          } else if (text) {
+            // We received a text message
+            // Let's run /message on the text to extract some entities, intents and traits
+            wit.message(text).then(({entities, intents, traits}) => {
+              // You can customize your response using these
+              console.log(intents);
+              console.log(entities);
+              console.log(traits);
+              // For now, let's reply with another automatic message
+              fbMessage(sender, `We've received your message: ${text}.`);
+            })
+            .catch((err) => {
+              console.error('Oops! Got an error from Wit: ', err.stack || err);
+            })
+          }
+        } else {
+          console.log('received event', JSON.stringify(event));
+        }
+      });
+    });
+  }
+  res.sendStatus(200);
+});
 
 /*
-function handleMessage(sender_psid, received_message) {
-  console.log("Start of handleMessage");
-  let response;
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+  console.log(signature);
 
-  // Checks if the message contains text
-  if (received_message.text) {
-    // Creates the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    }
-  } else if (received_message.attachments)
-  
-  /*VOICE MESSET
-  
-  else if (received_message.attachments) {
-    // Gets the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    console.log("attachment_url", attachment_url);
-    let audioObj = new Audio(attachment_url);
-    console.log("audioObj", audioObj);
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
 
-    // fetch(attachment_url)
-    // .then(response => response.blob())
-    // .then(blob => {
-    //   // use blob here...
-    //   console.log('blob', blob);
-    // });
+    var expectedHash = crypto.createHmac('sha1', FB_APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
 
-
-    const url = "https://api.wit.ai/speech";
-    const witToken = process.env.WIT_TOKEN; //don't put your token inline
-    // console.log("audioObj", audioObj);
-
-      console.log("buffer", buffer);
-
-      axios
-        .post(url, buffer, {
-          headers: {
-            Authorization: "Bearer " + witToken,
-            "Content-Type": "audio/wav"
-          }
-        })
-
-        .then(witResponse => {
-          console.log("wit response: " + JSON.stringify(witResponse.data));
-          // res.json(witResponse.data);
-        })
-
-        .catch(e => {
-          console.log("error sending to wit: " + e);
-          // res.json({ error: e.message });
-        });
-    }
-    
-    // Sends the response message
-    // callSendAPI(sender_psid, response); 
-
-    // check greeting is here and is confident
-    const greeting = firstTrait(received_message.nlp, 'wit$greetings');
-    console.log("GREETING", greeting);
-
-    console.log("received_message", received_message);
-    // console.log("CallSENDAPI", callSendAPI());
-
-    handler.responseFromWit
-    if (greeting && greeting.confidence > 0.8) {
-      callSendAPI(sender_psid, 'Hi there!');
-    } else {
-      // default
-      callSendAPI(sender_psid, 'default');
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
     }
   }
-} 
+}
 
-*/
+app.listen(PORT);
+console.log('Listening on :' + PORT + '...');
