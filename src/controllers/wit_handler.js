09279faeac41e07e85cf66182cb02ad9f0cee8b3
Antonio Mendieta <>
2021-05-09 19:58:08 +0000

    testing a new method

--- src/controllers/wit_handler.js
+++ src/controllers/wit_handler.js
@@ -1,22 +1,15 @@
 //main function for all messages
-export function responseFromWit(intentData, entities, traits) {
-  if(intentData[0]){
+export function responseFromWit(intentData, entitiesData, traitsData) {
+  if (intentData[0]){
     const intent = intentData[0];
     const intentname = intentData[0].name;
     const intentconfidence = intentData[0].confidence;
-  }
-  else{
-    return handleGibberish();
-  }
 
-  console.log("data from wit (wit_handler.js):");
-
-  let result = 'default';
-  switch (entities[0]) {
-    case "wit$greetings":
+    switch (intentname) {
+    case "greetings":
       return handleGreeting();
       break;
-    case "wit$bye":
+    case "bye":
       return "Thank you for your interest in Comida For Familias. Have a great day!"; //make to function
       break;
     case "donate":
@@ -51,14 +44,83 @@ export function responseFromWit(intentData, entities, traits) {
       break;
   }
 
+  else if(traitsData[0]){
+    if(traitsData[0]['wit$bye']){
+      const traitsName = "bye"
+    }
+    else{
+      const traitsName = "greetings"
+    }
+    
+    switch (traitsName) {
+    case "greetings":
+      return handleGreeting();
+      break;
+    case "bye":
+      return "Thank you for your interest in Comida For Familias. Have a great day!"; //make to function
+      break;
+  }
+  else {
+    return handleGibberish();
+  }
+
+  //testing
+  const testingintent = intentData.length > 0 && intentsData[0] || "__foo__";
+  const greetingtest = entities['wit$greetings:greetings'];
+
+  console.log(testingintent);
+  console.log(greetingstest)
+
+
+  console.log("data from wit (wit_handler.js):");
+
+  let result = 'default';
+  /*switch (intentname) {
+    case "greetings":
+      return handleGreeting();
+      break;
+    case "bye":
+      return "Thank you for your interest in Comida For Familias. Have a great day!"; //make to function
+      break;
+    case "donate":
+      return handleDonate();
+      break;
+    case "get_job_list":
+      return handleGetJobList();
+      break;
+    case "get_location":
+      return handleGetLocation();
+      break;
+    case "get_news":
+      return handleGetNews();
+      break;
+    case "get_projects":
+      return handleGetProjects();
+      break;
+    case "introduction":
+      return handleIntroduction();
+      break;
+    case "join_volunteer":
+      return handleJoinVolunteer();
+      break;
+    case "no_prob":
+      return "You are very welcome."; //make to function
+      break;
+    case "opt_cpt":
+      return handleOptCpt();
+      break;
+    case "organization_purpose":
+      return handleOrganizationPurpose();
+      break;*/
+  }
+
   //return handleGibberish();
 }
 
 
 //function for handling meaningless messages
 export function handleGibberish() {
-  return Promise.resolve(
-    "ask me something like 'How do I join as a volunteer?' or 'What is Comida For Familias for?'"
+  return "ask me something like 'How do I join as a volunteer?' or 'What is Comida For Familias for?'"
   );
 }
 
