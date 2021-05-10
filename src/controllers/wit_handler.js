3a1554d3cdeb397cfb7a0709d7f611cf77d337c8
Antonio Mendieta <>
2021-05-10 00:22:47 +0000

    update quick fix

--- src/controllers/wit_handler.js
+++ src/controllers/wit_handler.js
@@ -7,18 +7,18 @@
 export function responseFromWit(intentData, entitiesData, traitsData) {
   //testing
   const testingintent = intentData.length > 0 && intentData[0] || "__foo__";
-  
+
   console.log("TESTING: INTENT", testingintent);
   console.log("intentData[0]: ", intentData[0]);
   console.log("TraitsData: ", traitsData);
   //let boolean = traitsData.length > 0;
   console.log("BOOLEAN RESULT: ", traitsData['wit$bye'] && traitsData['wit$greetings']);
 
-  if (testingintent == '__foo__'){
+  if (testingintent == '__foo__') {
     return handleGibberish();
   }
 
-  else if (intentData[0]){
+  else if (intentData[0]) {
     const intent = intentData[0];
     const intentname = intentData[0].name;
     const intentconfidence = intentData[0].confidence;
@@ -61,43 +61,43 @@ export function responseFromWit(intentData, entitiesData, traitsData) {
       case "organization_purpose":
         return handleOrganizationPurpose();
         break;
-      }
+    }
     //}
   }
 
-  else if(traitsData != {}){
+  else if (traitsData != {}) {
     let traitsName = "default"
     let confidenceBye = "default"
     let confidenceGreeting = "default"
 
-    if(traitsData['wit$bye'] && traitsData['wit$greetings']){
+    if (traitsData['wit$bye'] && traitsData['wit$greetings']) {
       confidenceBye = traitsData['wit$bye']['confidence']
       confidenceGreeting = traitsData['wit$greetings']['confidence']
-      
-      if(confidenceBye > confidenceGreeting){
+
+      if (confidenceBye > confidenceGreeting) {
         traitsName = "bye"
       }
-      else{
+      else {
         traitsName = "greetings"
       }
     }
 
-    else if(traitsData['wit$bye'] && !traitsData['wit$greetings']){
+    else if (traitsData['wit$bye'] && !traitsData['wit$greetings']) {
       traitsName = "bye"
     }
 
-    else{
+    else {
       traitsName = "greetings"
     }
-    
-    
+
+
     switch (traitsName) {
-    case "greetings":
-      return handleGreeting();
-      break;
-    case "bye":
-      return "Thank you for your interest in Comida For Familias. Have a great day!"; //make to function
-      break;
+      case "greetings":
+        return handleGreeting();
+        break;
+      case "bye":
+        return "Thank you for your interest in Comida For Familias. Have a great day!"; //make to function
+        break;
     }
   }
   /*else {
@@ -164,15 +164,15 @@ function handleGreeting() {
   //console.log(greetingstest);
 
   const time = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  
+
   let greeting = ""
 
   console.log("TIME: ", time)
-  if (time < 11){
-     greeting = "good morning"
-  } else if (time < 18){
+  if (time < 11) {
+    greeting = "good morning"
+  } else if (time < 18) {
     greeting = "good afternoon"
-  } else if (time < 20){
+  } else if (time < 20) {
     greeting = "good evening"
   } else {
     greeting = "Hello there"
@@ -187,7 +187,7 @@ function handleGreeting() {
 function handleDonate() {
   let paypal = "Paypal";
   let paypalLink = "https://www.paypal.com/us/fundraiser/charity/3792494";
-  
+
   let message = "The best way to donate to us is using Paypal.\n" + paypalLink;
 
   return message;
@@ -205,7 +205,7 @@ function handleGetJobList() {
   });
 */
   let pageLink = "https://comidaforfamilias.org/getinvolved/positions";
-  
+
   let message = "Positions available include recruiter, marketer, developer, designer, data scientist and so much more. Find out more about these position by clicking the link: \n" + pageLink;
 
   return message;
@@ -218,22 +218,26 @@ function handleGetLocation() {
 
 function handleGetNews() {
   let link = "https://comidaforfamilias.org/newsevents/news";
-  
-  let message = "We have several articles and videos uploaded, including:\nFigma Fundamentals: Baseline Grids\n[Algorithms In Brief]: What Is Binary Search?\nCalifornia Ballot Propositions With PEP\n"+"To find out more, please click on the following link:\n";
-  
+
+  let message = "We have several articles and videos uploaded, including:\nFigma Fundamentals: Baseline Grids\n[Algorithms In Brief]: What Is Binary Search?\nCalifornia Ballot Propositions With PEP\n" + "To find out more, please click on the following link:\n";
+
   return message + link;
 }
+
+
 function handleGetProjects() {
   let pageLink = "https://comidaforfamilias.org/projectList";
 
   let message = "";
-  
+
   //message if they ask for current projects
   message = "Some projects we are currently working on are: \nTasteMatch\nCalorieHacker\nA Messenger bot using wit.ai\n" + "To Find out more click on the link.\n" + pageLink;
   //message if it is finished projects use this other message
 
   return message;
 }
+
+
 function handleIntroduction() {
   let message = "";
 
@@ -246,13 +250,19 @@ function handleJoinVolunteer() {
   let message = "To join as a volunteer you will have to find the position you want to be supervised for and fill out a form. ";
   return message;
 }
+
+
 function handleOptCpt() {
   let link = "https://comidaforfamilias.org/f1opt"
   let message = "F-1 students can volunteer and train with Comida For Familias, Inc. to maintain status and extend their stay in the US. The service volunteers/trainees provide must be related to the foreign national's degree program.\n" + "For more details, please click the following link:\n";
+
   return message + link;
 }
+
+
 function handleOrganizationPurpose() {
   let pageLink = "https://comidaforfamilias.org";
+  let message = "";
 
   message = "Our purpose is to use the talent of skilled individuals to connect the community with food resources with the use of technology. You can find out what percent we dedicate to scientific, educational, and charitable resources on our website:\n" + pageLink;
 
